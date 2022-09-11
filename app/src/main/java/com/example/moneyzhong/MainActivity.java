8dd537ca5f0b4086847b70d457bae7253b7d9384
‘mawl2017 <18701062981@163.com>
2022-09-11 16:21:25 +0800

    哈哈

--- app/src/main/java/com/example/moneyzhong/MainActivity.java
+++ app/src/main/java/com/example/moneyzhong/MainActivity.java
@@ -55,13 +55,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_sel_one:
                 String strOne=getOne();
 
-                lotteryTicket();
+//                lotteryTicket();
+
+                fucai();
                 break;
             case R.id.btn_sel_two:
-
+                daletou();
                 break;
             case R.id.btn_sel_threen:
-
+                qixingcai();
                 break;
 
         }
@@ -101,6 +103,90 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String str=(r==16? " + ": " ")+ n;
         Log.d("info","测试数据是=="+str);
         return seq + n + ",";
+    }
+
+    //+++++++++++++++++++++++++++++
+    /**
+     * 七星彩 机选方法
+     */
+    private void qixingcai() {
+        int[] qixingcai = new int[7];
+        for (int i = 0; i < 7; i++) {
+            qixingcai[i] = new Random().nextInt(10);
+        }
+        tv_threen.setText("七星彩：" + Arrays.toString(qixingcai));
+    }
+
+    /**
+     * 大乐透 机选方法
+     */
+    private void daletou() {
+        int[] r = new int[35];
+        int[] b = new int[12];
+        for (int i = 0; i < 35; i++) {
+            r[i] = i + 1;
+        }
+        for (int i = 0; i < 12; i++) {
+            b[i] = i + 1;
+        }
+
+        int[] red = new int[5];
+        boolean[] flag = new boolean[35];
+        for (int i = 0; i < 5; i++) {
+            int j;
+            do {
+                j = new Random().nextInt(35);
+            } while (flag[j]);
+            red[i] = r[j];
+            flag[j] = true;
+        }
+        int[] blue = new int[2];
+        boolean[] flag1 = new boolean[12];
+        for (int i = 0; i < 2; i++) {
+            int j;
+            do {
+                j = new Random().nextInt(12);
+            } while (flag[j]);
+            blue[i] = b[j];
+            flag[j] = true;
+        }
+        Arrays.sort(blue);
+        Arrays.sort(red);
+
+        tv_two.setText("红球：" + Arrays.toString(red) + "-蓝球：" + Arrays.toString(blue));
+    }
+
+    /**
+     * 福彩机选 方法
+     */
+    private void fucai() {
+        int[] r = new int[33];
+        int[] b = new int[16];
+
+        for (int i = 0; i < 33; i++) {
+            r[i] = i + 1;
+        }
+        for (int k = 0; k < 16; k++) {
+            b[k] = k + 1;
+        }
+        //在后台日志（logcat）中显示数组的值
+
+
+        int[] red = new int[6];
+        boolean[] flag = new boolean[33];//默认为false
+        for (int i = 0; i < 6; i++) {
+            int j;
+            do {//r数组中随机选下标赋值给j
+                j = new Random().nextInt(33);
+            } while (flag[j]);//当j位置选过回去重新选
+            red[i] = r[j];//选中标记成“已经选”
+            flag[j] = true;
+        }
+
+        int blue = b[new Random().nextInt(16)];
+        Arrays.sort(red);
+        tv_one.setText("红球：" + Arrays.toString(red) + "-" + "蓝球：" + blue);
+
     }
 
 }
